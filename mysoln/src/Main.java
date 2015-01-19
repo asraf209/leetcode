33db40280bc1216c016cd4f168145aeb65b857b1
Md Asraful Alom <malom@luc.edu>
2015-01-18 22:35:55 -0600

    solved longest non-reapting substring problem

---
+++ mysoln/src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by asraful on 1/18/15.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println(P4.getLongestLength("abcabcddbb"));
+    }
+}
