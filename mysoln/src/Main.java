45199746c4465b9de70bc71f754b265acf44cee3
Md Asraful Alom <malom@luc.edu>
2015-01-18 22:40:30 -0600

    renaming

--- mysoln/src/Main.java
+++ mysoln/src/Main.java
@@ -3,6 +3,6 @@
  */
 public class Main {
     public static void main(String[] args){
-        System.out.println(P4.getLongestLength("abcabcddbb"));
+        System.out.println(P3.getLongestLength("abcabcddbb"));
     }
 }
