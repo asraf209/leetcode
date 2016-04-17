5cea1d49e9daa3234d7f4aedf209d8035a7f183a
Md Asraful Alom <malom@luc.edu>
2016-04-17 14:28:27 -0500

    renamed method

--- mysolutions/src/twosum/Solution.java
+++ mysolutions/src/twosum/Solution.java
@@ -8,7 +8,7 @@ public class Solution {
 	 * @param target
 	 * @return
 	 */
-	public static int[] findTarget(int[] a, int target){
+	public static int[] findTargetTrivial(int[] a, int target){
 		if(a==null || a.length==0)	return null;
 		
 		int[] out = new int[2];
@@ -31,7 +31,7 @@ public class Solution {
 		// TODO Auto-generated method stub
 		int[] a = {2, 7, 11, 15}; 
 		int target = 9;
-		int[] out = findTarget(a, target);
+		int[] out = findTargetTrivial(a, target);
 		assert(out[0] == 0);
 		assert(out[1] == 1);
 		System.out.println(out[0] + ", " + out[1]);
