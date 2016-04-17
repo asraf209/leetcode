9f28dc272e13f312230b579c83612ee529d253fa
Md Asraful Alom <malom@luc.edu>
2016-04-17 14:39:39 -0500

    added test in main

--- mysolutions/src/twosum/Solution.java
+++ mysolutions/src/twosum/Solution.java
@@ -61,10 +61,18 @@ public class Solution {
 		// TODO Auto-generated method stub
 		int[] a = {2, 7, 11, 15}; 
 		int target = 9;
-		int[] out = findTargetTrivial(a, target);
-		assert(out[0] == 0);
-		assert(out[1] == 1);
-		System.out.println(out[0] + ", " + out[1]);
+		
+		// Trivial one
+		int[] trivialOut = findTargetTrivial(a, target);
+		assert(trivialOut[0] == 0);
+		assert(trivialOut[1] == 1);
+		System.out.println(trivialOut[0] + ", " + trivialOut[1]);
+		
+		// Better one
+		int[] betterOut = findTargetTrivial(a, target);
+		assert(betterOut[0] == 0);
+		assert(betterOut[1] == 1);
+		System.out.println(betterOut[0] + ", " + betterOut[1]);
 	}
 
 }
