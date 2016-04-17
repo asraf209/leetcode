5df02369dd7b5071d265c574cb58e54ce8122757
Md Asraful Alom <malom@luc.edu>
2016-04-17 17:00:32 -0500

    code for edge case

--- mysolutions/src/threesum/Solution.java
+++ mysolutions/src/threesum/Solution.java
@@ -1,13 +1,26 @@
 package threesum;
 
-public class Solution {
+import java.util.List;
 
+public class Solution {
+	
+	public static List<List<Integer>> get3Sum(int[] a, int target){
+		if(a==null || a.length<3)	return null;
+		return null;
+	}
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] a = {-1, 0, 1, 2, -1, -4}; 
+		int target = 0;
+				
+		/*int[] trivialOut = findTargetTrivial(a, target);
+		assert(trivialOut[0] == 0);
+		assert(trivialOut[1] == 1);
+		System.out.println(trivialOut[0] + ", " + trivialOut[1]);
+		*/
 	}
 
 }
