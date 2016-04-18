bf338a55b669cc142da6c5e65491b0f3df6295b4
Md Asraful Alom <malom@luc.edu>
2016-04-17 20:37:33 -0500

    optimized the new approach

--- mysolutions/src/threesum/Solution.java
+++ mysolutions/src/threesum/Solution.java
@@ -13,17 +13,34 @@ public class Solution {
 		
 		Arrays.sort(nums);
 		List<List<Integer>> threeSum = new ArrayList<>();
+		
 		for(int i=0; i<nums.length-2; i++){
+			
+			if(i>0 && nums[i] != nums[i-1])	continue;
+			
 			int start = i+1;
 			int end = nums.length-1;
-			while(start < end){
+			
+			while(start < end){								
+				if((start-1)!=i && (nums[start] == nums[start-1])){
+					start++;
+					continue;
+				}
+				else if((end+1)<nums.length && (nums[end] == nums[end+1])){
+					end--;
+					continue;
+				}
+				
 				int sum = nums[i] + nums[start] + nums[end];
+				
 				if(sum == target){
 					List<Integer> triplet = new ArrayList<>();
 					triplet.add(nums[i]);
 					triplet.add(nums[start]);
 					triplet.add(nums[end]);
 					threeSum.add(triplet);
+					start++;
+					end--;
 				}
 				else if(sum < target)	start++;
 				else end--;
@@ -98,11 +115,13 @@ public class Solution {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		int[] a = {0, 0, 0, 0};
 		//int[] a = {-1, 0, 1, 2, -1, -4};
-		int[] a = {13,4,-6,-7,-15,-1,0,-1,0,-12,-12,9,3,-14,-2,-5,-6,7,8,2,-4,6,-5,-10,-4,-9,-14,-14,12,-13,-7,3,7,2,11,7,9,-4,13,-6,-1,-14,-12,9,9,-6,-11,10,-14,13,-2,-11,-4,8,-6,0,7,-12,1,4,12,9,14,-4,-3,11,10,-9,-8,8,0,-1,1,3,-15,-12,4,12,13,6,10,-4,10,13,12,12,-2,4,7,7,-15,-4,1,-15,8,5,3,3,11,2,-11,-12,-14,5,-1,9,0,-12,6,-1,1,1,2,-3};
+		//int[] a = {7,-1,14,-12,-8,7,2,-15,8,8,-8,-14,-4,-5,7,9,11,-4,-15,-6,1,-14,4,3,10,-5,2,1,6,11,2,-2,-5,-7,-6,2,-15,11,-6,8,-4,2,1,-1,4,-6,-15,1,5,-15,10,14,9,-8,-6,4,-6,11,12,-15,7,-1,-9,9,-1,0,-4,-1,-12,-2,14,-9,7,0,-3,-4,1,-2,12,14,-10,0,5,14,-1,14,3,8,10,-8,8,-5,-2,6,-11,12,13,-7,-12,8,6,-13,14,-2,-5,-11,1,3,-6};
 		int target = 0;
 		long startTime = System.currentTimeMillis();
-		List<List<Integer>> threeSum = get3Sum(a, target);
+		//List<List<Integer>> threeSum = get3Sum(a, target);
+		List<List<Integer>> threeSum = get3Sum2ndApproach(a, target);
 		long endTime = System.currentTimeMillis();
 		//assert(threeSum.size() == 3);
 		System.out.println(endTime - startTime);
