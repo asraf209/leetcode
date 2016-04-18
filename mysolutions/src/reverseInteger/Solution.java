351b272278f38bd5bb34d2cf1ffeaecbf27bdb87
Asraf <asraf.alom02@gmail.com>
2016-04-18 20:40:56 +0000

    initial implementation

--- mysolutions/src/reverseInteger/Solution.java
+++ mysolutions/src/reverseInteger/Solution.java
@@ -4,7 +4,29 @@ package reverseInteger;
 
 public class Solution {
 
+	public static int reverse(int x){
 
+		if(x==0)	return x;		
+
+		int n = Math.abs(x);
+
+		int out = 0;
+
+		while(n/10!=0){			
+
+			int modN = n % 10;
+
+			out = 10*out + modN;
+
+			n = n / 10;
+
+		}
+
+		if(x<0)	return (10*out + n)*(-1);
+
+		return 10*out + n;
+
+	}
 
 	/**
 
