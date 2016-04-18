1319f0f8398e176e80a5a46b09c0dda4d7c9c61b
Md Asraful Alom <malom@luc.edu>
2016-04-17 20:01:05 -0500

    refactored

--- mysolutions/src/threesum/Solution.java
+++ mysolutions/src/threesum/Solution.java
@@ -7,30 +7,37 @@ import java.util.Set;
 
 public class Solution {
 	
-	public static ArrayList<ArrayList<Integer>> get3Sum(int[] a, int target){
-		if(a==null || a.length<3)	return null;		
+	
+	/**
+	 * O(n^2). Time limit exceeded
+	 * @param nums
+	 * @param target
+	 * @return
+	 */
+	public static List<List<Integer>> get3Sum(int[] nums, int target){
+		if(nums==null || nums.length<3)	return new ArrayList<>();		
 		
 		List<List<Integer>> twoSum = new ArrayList<>();
-		for(int i=0; i<a.length-1; i++){			
-			for(int j=i+1; j<a.length; j++){
+		for(int i=0; i<nums.length-1; i++){			
+			for(int j=i+1; j<nums.length; j++){
 				List<Integer> pair = new ArrayList<>();
-				if(a[i]<=a[j]){
-					pair.add(a[i]);
-					pair.add(a[j]);
+				if(nums[i]<=nums[j]){
+					pair.add(nums[i]);
+					pair.add(nums[j]);
 				}
 				else{
-					pair.add(a[j]);
-					pair.add(a[i]);
+					pair.add(nums[j]);
+					pair.add(nums[i]);
 				}
 				twoSum.add(pair);
 			}
 		}
 		
-		Set<ArrayList<Integer>> threeSum = new HashSet<ArrayList<Integer>>();
+		Set<List<Integer>> threeSum = new HashSet<List<Integer>>();
 		for(List<Integer> pair : twoSum){			
-			for(int n : a){				
+			for(int n : nums){				
 				if(doSum(pair, n) == 0){					
-					ArrayList<Integer> triplet = new ArrayList<Integer>();
+					List<Integer> triplet = new ArrayList<Integer>();
 					if(n<=pair.get(0)){
 						triplet.add(n);
 						triplet.add(pair.get(0));
@@ -52,7 +59,8 @@ public class Solution {
 			}
 		}					
 		
-		return new ArrayList<ArrayList<Integer>>(threeSum);
+		List<List<Integer>> out = new ArrayList<>(threeSum);		
+		return out;//new List<List<Integer>>(threeSum);
 	}
 	
 	private static int doSum(List<Integer> pair, int n){			
@@ -65,10 +73,14 @@ public class Solution {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int[] a = {-1, 0, 1, 2, -1, -4}; 
+		//int[] a = {-1, 0, 1, 2, -1, -4};
+		int[] a = {13,4,-6,-7,-15,-1,0,-1,0,-12,-12,9,3,-14,-2,-5,-6,7,8,2,-4,6,-5,-10,-4,-9,-14,-14,12,-13,-7,3,7,2,11,7,9,-4,13,-6,-1,-14,-12,9,9,-6,-11,10,-14,13,-2,-11,-4,8,-6,0,7,-12,1,4,12,9,14,-4,-3,11,10,-9,-8,8,0,-1,1,3,-15,-12,4,12,13,6,10,-4,10,13,12,12,-2,4,7,7,-15,-4,1,-15,8,5,3,3,11,2,-11,-12,-14,5,-1,9,0,-12,6,-1,1,1,2,-3};
 		int target = 0;
-		ArrayList<ArrayList<Integer>> threeSum = get3Sum(a, target);
-		assert(threeSum.size() == 3);		
+		long startTime = System.currentTimeMillis();
+		List<List<Integer>> threeSum = get3Sum(a, target);
+		long endTime = System.currentTimeMillis();
+		//assert(threeSum.size() == 3);
+		System.out.println(endTime - startTime);
 		System.out.println(threeSum);			
 	}
 
