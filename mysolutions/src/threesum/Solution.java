60348e7479f0817e11c37dd1235669e9e45bd576
Md Asraful Alom <malom@luc.edu>
2016-04-17 20:38:05 -0500

    optimized the new approach

--- mysolutions/src/threesum/Solution.java
+++ mysolutions/src/threesum/Solution.java
@@ -16,7 +16,7 @@ public class Solution {
 		
 		for(int i=0; i<nums.length-2; i++){
 			
-			if(i>0 && nums[i] != nums[i-1])	continue;
+			if(i>0 && nums[i] == nums[i-1])	continue;
 			
 			int start = i+1;
 			int end = nums.length-1;
