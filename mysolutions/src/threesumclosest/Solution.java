245ada5e7edbce0e6ce305fefcee75059a38f07b
Md Asraful Alom <malom@luc.edu>
2016-04-17 21:54:58 -0500

    implemented with two pointers

--- mysolutions/src/threesumclosest/Solution.java
+++ mysolutions/src/threesumclosest/Solution.java
@@ -15,11 +15,13 @@ public class Solution {
 	 * @param target
 	 * @return
 	 */
-	public static List<List<Integer>> get3Sum2ndApproach(int[] nums, int target){
-		if(nums==null || nums.length<3)	return new ArrayList<>();
-		
+	public static int get3SumClosest(int[] nums, int target){
+		if(nums==null || nums.length<3)	return 0;
+				
 		Arrays.sort(nums);
-		List<List<Integer>> threeSum = new ArrayList<>();
+		
+		int closestDiff = Integer.MAX_VALUE;
+		int a=0, b=0, c=0;		
 		
 		for(int i=0; i<nums.length-2; i++){
 			
@@ -40,98 +42,39 @@ public class Solution {
 				
 				int sum = nums[i] + nums[start] + nums[end];
 				
-				if(sum == target){
-					List<Integer> triplet = new ArrayList<>();
-					triplet.add(nums[i]);
-					triplet.add(nums[start]);
-					triplet.add(nums[end]);
-					threeSum.add(triplet);
+				if(sum == target){					
+					System.out.println(a + ", " + b + ", " + c);
+					return a + b + c;
+				}
+				else if(Math.abs(target-sum) < closestDiff){
+					a = nums[i];
+					b = nums[start];
+					c = nums[end];
+					closestDiff = Math.abs(target-sum);
 					start++;
-					end--;
+					end--;					
 				}
-				else if(sum < target)	start++;
-				else end--;
+				else	end--;
 			}
 		}
-		
-		return threeSum;
+				
+		return a+b+c;
 	}
 	
-	/**
-	 * O(n^2). Time limit exceeded
-	 * @param nums
-	 * @param target
-	 * @return
-	 */
-	public static List<List<Integer>> get3Sum(int[] nums, int target){
-		if(nums==null || nums.length<3)	return new ArrayList<>();		
-		
-		List<List<Integer>> twoSum = new ArrayList<>();
-		for(int i=0; i<nums.length-1; i++){			
-			for(int j=i+1; j<nums.length; j++){
-				List<Integer> pair = new ArrayList<>();
-				if(nums[i]<=nums[j]){
-					pair.add(nums[i]);
-					pair.add(nums[j]);
-				}
-				else{
-					pair.add(nums[j]);
-					pair.add(nums[i]);
-				}
-				twoSum.add(pair);
-			}
-		}
-		
-		Set<List<Integer>> threeSum = new HashSet<List<Integer>>();
-		for(List<Integer> pair : twoSum){			
-			for(int n : nums){				
-				if(doSum(pair, n) == 0){					
-					List<Integer> triplet = new ArrayList<Integer>();
-					if(n<=pair.get(0)){
-						triplet.add(n);
-						triplet.add(pair.get(0));
-						triplet.add(pair.get(1));
-					}
-					else if(n>=pair.get(1)){						
-						triplet.add(pair.get(0));
-						triplet.add(pair.get(1));
-						triplet.add(n);
-					}
-					else{
-						triplet.add(pair.get(0));
-						triplet.add(n);
-						triplet.add(pair.get(1));						
-					}	
-					if(!threeSum.contains(triplet))
-						threeSum.add(triplet);
-				}
-			}
-		}					
-		
-		List<List<Integer>> out = new ArrayList<>(threeSum);		
-		return out;
-	}
-	
-	private static int doSum(List<Integer> pair, int n){			
-		int sum = n;
-		for(int m : pair)	sum += m;
-		return sum;
-	}
 	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		//int[] a = {0, 0, 0, 0};
-		int[] a = {-1, 0, 1, 2, -1, -4};
-		//int[] a = {7,-1,14,-12,-8,7,2,-15,8,8,-8,-14,-4,-5,7,9,11,-4,-15,-6,1,-14,4,3,10,-5,2,1,6,11,2,-2,-5,-7,-6,2,-15,11,-6,8,-4,2,1,-1,4,-6,-15,1,5,-15,10,14,9,-8,-6,4,-6,11,12,-15,7,-1,-9,9,-1,0,-4,-1,-12,-2,14,-9,7,0,-3,-4,1,-2,12,14,-10,0,5,14,-1,14,3,8,10,-8,8,-5,-2,6,-11,12,13,-7,-12,8,6,-13,14,-2,-5,-11,1,3,-6};
-		int target = 0;
-		long startTime = System.currentTimeMillis();
-		//List<List<Integer>> threeSum = get3Sum(a, target);
-		List<List<Integer>> threeSum = get3Sum2ndApproach(a, target);
+
+		int[] a = {-1, 2, 1, -4};
+		int target = 1;
+
+		long startTime = System.currentTimeMillis();		
+		int out = get3SumClosest(a, target);
 		long endTime = System.currentTimeMillis();	
 		System.out.println(endTime - startTime);
-		System.out.println(threeSum);			
+		System.out.println(out);	
 	}
 
 }
