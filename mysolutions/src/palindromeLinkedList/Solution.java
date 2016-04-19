628818d8e8363d73a56f951f12937228ba8b97d2
Md Asraful Alom <malom@luc.edu>
2016-04-19 00:32:32 -0500

    implemented method

--- mysolutions/src/palindromeLinkedList/Solution.java
+++ mysolutions/src/palindromeLinkedList/Solution.java
@@ -1,5 +1,7 @@
 package palindromeLinkedList;
 
+import org.w3c.dom.NodeList;
+
 public class Solution {
 	class ListNode {
 		int val;
@@ -7,23 +9,48 @@ public class Solution {
 		ListNode(int x) { val = x; }
 	}
 	
-	@SuppressWarnings("null")
-	public boolean isPalindrome(ListNode head) {
-		if(head==null)	return false;
-		if(head.next==null)	return true;
+	public boolean isPalindrome(ListNode head) {		
+		if(head==null || head.next==null)	return true;
 		
-		// Find the middle element to separate into two list
+		// Find middle element to separate into two list
 		ListNode slow = head;
 		ListNode fast = head;
-		while(fast != null || fast.next!=null){
+		while(fast != null && fast.next!=null){
 			slow = slow.next;
 			fast = fast.next.next;
 		}		
 		if(fast.next==null)		// Odd number of items
 			slow = slow.next;	
 		
+		// Make two list
+		ListNode leftHead = head;
+		ListNode rightHead = null;
+		ListNode rightTail = rightHead;
 		
-		return false;
+		// Reverse the right half and make a LinkedList
+		while(slow!=null){
+			ListNode tmp = slow;
+			slow = slow.next;
+			tmp.next = null;
+			
+			if(rightHead == null){
+				rightHead = tmp;
+				rightTail = tmp;
+			}
+			else{
+				tmp.next = rightHead;
+				rightHead = tmp;
+			}			
+		}
+		
+		// Compare the two LinkedList to find if it is palindrome or not
+		ListNode rightPointer = rightHead;
+		while(leftHead.next!=rightHead || leftHead.next.next!=rightHead){
+			if(leftHead.val != rightPointer.val)	return false;
+			leftHead = leftHead.next;
+			rightPointer = rightPointer.next;
+		}		
+		return true;
     }
 
 	/**
