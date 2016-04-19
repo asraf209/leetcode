c172c9e750e4587b057e6c91ac04f846bae5b6c7
Md Asraful Alom <malom@luc.edu>
2016-04-18 23:37:37 -0500

    found middle element

--- mysolutions/src/palindromeLinkedList/Solution.java
+++ mysolutions/src/palindromeLinkedList/Solution.java
@@ -1,6 +1,30 @@
 package palindromeLinkedList;
 
 public class Solution {
+	class ListNode {
+		int val;
+		ListNode next;
+		ListNode(int x) { val = x; }
+	}
+	
+	@SuppressWarnings("null")
+	public boolean isPalindrome(ListNode head) {
+		if(head==null)	return false;
+		if(head.next==null)	return true;
+		
+		// Find the middle element to separate into two list
+		ListNode slow = head;
+		ListNode fast = head;
+		while(fast != null || fast.next!=null){
+			slow = slow.next;
+			fast = fast.next.next;
+		}		
+		if(fast.next==null)		// Odd number of items
+			slow = slow.next;	
+		
+		
+		return false;
+    }
 
 	/**
 	 * @param args
