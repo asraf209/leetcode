830240053fc29b7835b5d3341f3dcc17d438466c
Md Asraful Alom <malom@luc.edu>
2016-04-22 23:00:10 -0500

    done

--- mysolutions/src/evaluateReversePolishNotation/SolutionERPN.java
+++ mysolutions/src/evaluateReversePolishNotation/SolutionERPN.java
@@ -3,8 +3,10 @@ package evaluateReversePolishNotation;
 import java.util.Stack;
 
 public class SolutionERPN {
-	public int evalRPN(String[] tokens) {
+	
+	public static  int evalRPN(String[] tokens) {
 		if(tokens == null || tokens.length==0)	return 0;
+		
 		int a, b;
 		Stack<Integer> stack = new Stack<Integer>();		
 		for(String s : tokens){
@@ -42,7 +44,8 @@ public class SolutionERPN {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String[] tokens = {"4", "13", "5", "/", "+"}; //{"2", "1", "+", "3", "*"};
+		System.out.println(evalRPN(tokens));
 	}
 
 }
