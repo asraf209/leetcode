5314ad691eba843424c90a39b01c496247b66ab5
Md Asraful Alom <malom@luc.edu>
2016-04-22 22:43:11 -0500

    implementing method

--- mysolutions/src/evaluateReversePolishNotation/SolutionERPN.java
+++ mysolutions/src/evaluateReversePolishNotation/SolutionERPN.java
@@ -1,6 +1,41 @@
 package evaluateReversePolishNotation;
 
+import java.util.Stack;
+
 public class SolutionERPN {
+	public int evalRPN(String[] tokens) {
+		if(tokens == null || tokens.length==0)	return 0;
+		int a, b;
+		Stack<Integer> stack = new Stack<Integer>();		
+		for(String s : tokens){
+			switch(s){
+				case "/":
+					b = stack.pop();
+					a = stack.pop();
+					stack.push(a/b);
+					break;
+				case "*":
+					b = stack.pop();
+					a = stack.pop();
+					stack.push(a*b);
+					break;
+				case "+":
+					b = stack.pop();
+					a = stack.pop();
+					stack.push(a+b);
+					break;
+				case "-":
+					b = stack.pop();
+					a = stack.pop();
+					stack.push(a-b);
+					break;
+				default:
+					stack.push(Integer.parseInt(s));
+					break;
+			}					
+		}
+		return stack.pop();
+    }
 
 	/**
 	 * @param args
