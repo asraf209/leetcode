da0023d1f630dcde69d2a0984d7ccbde38048352
Md Asraful Alom <malom@luc.edu>
2016-04-24 22:22:53 -0500

    implemented method

--- mysolutions/src/reverseString/ReverseString.java
+++ mysolutions/src/reverseString/ReverseString.java
@@ -1,13 +1,24 @@
 package reverseString;
 
 public class ReverseString {
+	public static String reverseString(String s) {
+		if(s==null || s.trim().isEmpty())	return null;
+		char[] ch = s.toCharArray();
+		for(int i=0, j=s.length()-1; i<j; i++, j--){
+			char tmp = ch[i];
+			ch[i] = ch[j];
+			ch[j] = tmp;
+		}
+        return new String(ch);
+    }
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println(reverseString("hello"));
 	}
 
 }
+	
