3b98b4684e92454bc58b4ffb3449b88acb58b0a6
Md Asraful Alom <malom@luc.edu>
2016-04-28 02:27:11 -0500

    implementing

--- mysolutions/src/removeNthNode/RemoveNthNode.java
+++ mysolutions/src/removeNthNode/RemoveNthNode.java
@@ -1,7 +1,38 @@
 package removeNthNode;
 
 public class RemoveNthNode {
+	public class ListNode {
+		 int val;
+		 ListNode next;
+		 ListNode(int x) { val = x; }
+	}
 	
+	public ListNode removeNthFromEnd(ListNode head, int n) {
+		if(head==null || n<1)	return head;
+		ListNode p1 = head;
+		ListNode p2 = head;	
+		
+		/**
+		 * Make a sublist n1...n2, n node apart
+		 */
+		int i=1;
+		while(i<n && p2 != null){
+			p2 = p2.next;
+			i++;
+		}
+		
+		if(p2==null)	return head;	// n > Size of the list
+		
+		/**
+		 * Move the sublist till the end
+		 */
+		while(p2.next != null){
+			p1 = p1.next;
+			p2 = p2.next;
+		}
+		
+		return head;
+    }
 	/**
 	 * @param args
 	 */
