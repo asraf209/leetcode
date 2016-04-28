74bf334991b7b29fb223f941398dc264753df2ae
Md Asraful Alom <malom@luc.edu>
2016-04-28 01:11:58 -0500

    added new method to make linked list

--- mysolutions/src/rotateList/RotateList.java
+++ mysolutions/src/rotateList/RotateList.java
@@ -5,19 +5,21 @@ public class RotateList {
 		 int val;
 		 ListNode next;
 		 ListNode(int x) { val = x; }
-	}
+	}		
 	
-	public ListNode returnList(){
-		ListNode n1 = new ListNode(1);
-		ListNode n2 = new ListNode(2);
-		ListNode n3 = new ListNode(3);
-		ListNode n4 = new ListNode(4);
-		ListNode n5 = new ListNode(5);
-		n1.next = n2;
-		n2.next = n3;
-		n3.next = n4;
-		n4.next = n5;
-		return n1;
+	public ListNode makeList(int[] a){
+		ListNode head=null, curr= null;
+		for(int n : a){
+			if(head==null){
+				head = new ListNode(n);
+				curr = head;
+			}
+			else{
+				curr.next = new ListNode(n);
+				curr = curr.next;
+			}
+		}		
+		return head;
 	}
 	
 	public ListNode rotateRight(ListNode head, int k) {
@@ -59,7 +61,8 @@ public class RotateList {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		RotateList obj = new RotateList();
-		ListNode head = obj.returnList();
+		int[] a = {1, 2};
+		ListNode head = obj.makeList(a);
 		ListNode retHead = obj.rotateRight(head, 2);
 		while(retHead != null){
 			System.out.println(retHead.val);
