1b6e1f5ed39fa45cf17e2f2fd5fdfa9874f60675
Md Asraful Alom <malom@luc.edu>
2016-04-28 02:31:56 -0500

    added main

--- mysolutions/src/removeNthNode/RemoveNthNode.java
+++ mysolutions/src/removeNthNode/RemoveNthNode.java
@@ -7,6 +7,21 @@ public class RemoveNthNode {
 		 ListNode(int x) { val = x; }
 	}
 	
+	public ListNode makeList(int[] a){
+		ListNode head=null, curr= null;
+		for(int n : a){
+			if(head==null){
+				head = new ListNode(n);
+				curr = head;
+			}
+			else{
+				curr.next = new ListNode(n);
+				curr = curr.next;
+			}
+		}		
+		return head;
+	}
+	
 	public ListNode removeNthFromEnd(ListNode head, int n) {
 		if(head==null || n<1)	return head;
 		ListNode p1 = head;
@@ -42,7 +57,14 @@ public class RemoveNthNode {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int[] a = {1, 2, 3, 4, 5};
+		RemoveNthNode obj = new RemoveNthNode();
+		ListNode head = obj.makeList(a);
+		ListNode retHead = obj.removeNthFromEnd(head, 2);
+		while(retHead != null){
+			System.out.println(retHead.val);
+			retHead = retHead.next;
+		}
 	}
 
 }
