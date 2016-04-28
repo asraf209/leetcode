b897d33bce99c96cb599b4bb7503f1a6ab60d2e1
Md Asraful Alom <malom@luc.edu>
2016-04-28 01:58:18 -0500

    implemented algorithm

--- mysolutions/src/rotateArray/RotateArray.java
+++ mysolutions/src/rotateArray/RotateArray.java
@@ -1,6 +1,20 @@
 package rotateArray;
 
 public class RotateArray {
+	public static void rotate(int[] nums, int k) {
+        if(nums==null || nums.length<2 || k<1)	return;
+        rotateXtoY(nums, 0, nums.length-1-k);
+        rotateXtoY(nums, nums.length-k, nums.length-1);
+        rotateXtoY(nums, 0, nums.length-1);
+    }
+	
+	private static void rotateXtoY(int[] nums, int x, int y){
+		for(int i=x, j=y; i<j; i++, j--){
+			int tmp = nums[i];
+			nums[i] = nums[j];
+			nums[j] = tmp;
+		}
+	}
 
 	/**
 	 * @param args
