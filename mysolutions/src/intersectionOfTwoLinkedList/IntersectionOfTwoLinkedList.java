74c8b3c9a8b6d9ec77432e820033ad56b865eb3d
Md Asraful Alom <malom@luc.edu>
2016-04-28 22:13:15 -0500

    cleanup

--- mysolutions/src/intersectionOfTwoLinkedList/IntersectionOfTwoLinkedList.java
+++ mysolutions/src/intersectionOfTwoLinkedList/IntersectionOfTwoLinkedList.java
@@ -9,23 +9,7 @@ public class IntersectionOfTwoLinkedList {
 			 val = x;
 		       next = null;
 		 }
-	}
-	
-	public ListNode makeList(int[] a){
-		ListNode head = null;
-		ListNode p = null;
-		for(int n : a){
-			if(head == null){
-				head = new ListNode(n);
-				p = head;
-			}
-			else{
-				p.next = new ListNode(n);
-				p = p.next;
-			}
-		}
-		return head;
-	}
+	}		
 	
 	public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
 		if(headA == null || headB == null)	return null;
@@ -70,33 +54,13 @@ public class IntersectionOfTwoLinkedList {
 		
 		return a;
     }
-	
-	public static void printList(ListNode head){
-		while(head != null){
-			System.out.print(head.val + "->");
-			head = head.next;
-		}
-		System.out.print("null");
-		System.out.println();
-	}
-	
+		
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		IntersectionOfTwoLinkedList obj = new IntersectionOfTwoLinkedList();
-		
-		int[] a = {1,2,3,4,5};
-		ListNode headA = obj.makeList(a);
-		printList(headA);
-		
-		int[] b = {6,7,8,3,4,5};
-		ListNode headB = obj.makeList(b);
-		printList(headB);
-		
-		ListNode comm = obj.getIntersectionNode(headA, headB);
-		printList(comm);
+	
 	}
 
 }
