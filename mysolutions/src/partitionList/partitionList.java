c1234780bae98b83d612dbed06f91ca1da96e50a
Md Asraful Alom <malom@luc.edu>
2016-04-28 22:30:31 -0500

    implemented method

--- mysolutions/src/partitionList/partitionList.java
+++ mysolutions/src/partitionList/partitionList.java
@@ -1,6 +1,42 @@
 package partitionList;
 
 public class partitionList {
+	public class ListNode {
+		 int val;
+		 ListNode next;
+		 ListNode(int x) { val = x; }
+	}
+	
+	public ListNode partition(ListNode head, int x) {
+		if(head==null || head.next==null)	return head;
+		
+		ListNode leftHead = null;
+		ListNode leftTail = null;		
+		ListNode rightHead = null;
+		ListNode rightTail = null;
+		
+		ListNode p = head;
+		
+		while(p!=null){
+			if(p.val < x)	addToList(leftHead, leftTail, p);
+			else	addToList(rightHead, rightTail, p);
+		}
+		
+		leftTail.next = rightHead;		
+		
+		return leftHead;
+    }
+	
+	public void addToList(ListNode head, ListNode tail, ListNode p){
+		if(head == null){
+			head = p;
+			tail = head;
+		}
+		else{
+			tail.next = p;
+			tail = tail.next;
+		}
+	}
 
 	/**
 	 * @param args
