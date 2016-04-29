a9274e1be67956a0a21b3a448b55ff8d82fec12e
Asraful Alom <asraf.alom02@gmail.com>
2016-04-29 02:50:47 -0500

    implemented algorithm

--- mysolutions/src/reorderList/ReorderList.java
+++ mysolutions/src/reorderList/ReorderList.java
@@ -1,6 +1,26 @@
 package reorderList;
 
 public class ReorderList {
+	class ListNode {
+		 int val;
+		 ListNode next;
+		 ListNode(int x) { val = x; }
+	}
+	
+	public void reorderList(ListNode head) {
+		if(head==null || head.next==null || head.next.next==null)	return;
+		ListNode p = head;
+		while(p.next==head || p.next.next!=head){
+			ListNode q = p.next;
+			while(q.next!=null)	q = q.next;
+			q.next = p.next;
+			p.next = q;
+			p = q.next;
+		}
+		
+		if(p.next==head)	p.next = null;
+		else if(p.next.next==head)	p.next.next = null;
+    }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
