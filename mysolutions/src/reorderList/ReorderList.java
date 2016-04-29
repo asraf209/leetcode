f18cff429e1ebdbbbc9fd8eade7566cc45531bf0
Asraful Alom <asraf.alom02@gmail.com>
2016-04-29 03:30:12 -0500

    fixed null pointer error

--- mysolutions/src/reorderList/ReorderList.java
+++ mysolutions/src/reorderList/ReorderList.java
@@ -10,21 +10,53 @@ public class ReorderList {
 	public void reorderList(ListNode head) {
 		if(head==null || head.next==null || head.next.next==null)	return;
 		ListNode p = head;
-		while(p.next==head || p.next.next!=head){
+		while(p.next!=null && p.next.next!=null){
 			ListNode q = p.next;
-			while(q.next!=null)	q = q.next;
+			ListNode tmp = p;
+			while(q.next!=null){				
+				q = q.next;
+				tmp = tmp.next;
+			}
 			q.next = p.next;
 			p.next = q;
-			p = q.next;
-		}
-		
-		if(p.next==head)	p.next = null;
-		else if(p.next.next==head)	p.next.next = null;
+			tmp.next = null;
+			p = q.next;			
+		}		
     }
 
+	public ListNode makeList(int[] a){
+		ListNode head=null, curr= null;
+		for(int n : a){
+			if(head==null){
+				head = new ListNode(n);
+				curr = head;
+			}
+			else{
+				curr.next = new ListNode(n);
+				curr = curr.next;
+			}
+		}		
+		return head;
+	}
+	
+	public static void printList(ListNode head){
+		while(head!=null){
+			System.out.print(head.val + "->");
+			head = head.next;
+		}
+		System.out.print("null");
+		System.out.println();
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int[] a = {1, 2, 3, 4, 5};
+		ReorderList obj = new ReorderList();
+		ListNode head = obj.makeList(a);
+		printList(head);
+		
+		obj.reorderList(head);
+		printList(head);
 	}
 
 }
