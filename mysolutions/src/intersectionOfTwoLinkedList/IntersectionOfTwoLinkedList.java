d48e1f7c37fb20d9ea08ba4fa2444087b885d120
Md Asraful Alom <malom@luc.edu>
2016-04-28 21:56:08 -0500

    implemented method

--- mysolutions/src/intersectionOfTwoLinkedList/IntersectionOfTwoLinkedList.java
+++ mysolutions/src/intersectionOfTwoLinkedList/IntersectionOfTwoLinkedList.java
@@ -1,7 +1,60 @@
 package intersectionOfTwoLinkedList;
 
 public class IntersectionOfTwoLinkedList {
-
+	
+	public class ListNode {
+		 int val;
+		 ListNode next;
+		 ListNode(int x) {
+			 val = x;
+		       next = null;
+		 }
+	}
+	
+	public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+		if(headA == null || headB == null)	return null;
+		
+		// COmpute size of list A
+		int sizeA = 0;
+		ListNode a = headA;
+		while(a!=null){
+			sizeA ++;
+			a = a.next;
+		}
+		
+		// Compute size of list B
+		int sizeB = 0;
+		ListNode b = headB;
+		while(b!=null){
+			sizeB ++;
+			b = b.next;
+		}
+		
+		// Move head pointer of Big List untill they become same size
+		a = headA;
+		b = headB;
+		if(sizeA > sizeB){
+			while(sizeA != sizeB){
+				a = a.next;
+				sizeA --;
+			}
+		}
+		else if(sizeB > sizeA){
+			while(sizeB != sizeA){
+				b = b.next;
+				sizeB --;
+			}
+		}
+			
+		// Now move both pointer at the same speed. They will meet eventually 
+		while(a!=b){
+			a = a.next;
+			b = b.next;
+		}
+		
+		return a;
+    }
+	
 	/**
 	 * @param args
 	 */
