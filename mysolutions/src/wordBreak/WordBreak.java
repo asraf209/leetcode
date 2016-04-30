f7fe0633ef3cbc9f928a42b3a39edd6f4603941e
Asraf <asraf.alom02@gmail.com>
2016-04-30 16:02:55 -0500

    fixed index issue

--- mysolutions/src/wordBreak/WordBreak.java
+++ mysolutions/src/wordBreak/WordBreak.java
@@ -1,20 +1,22 @@
 package wordBreak;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class WordBreak {
 	
-	public boolean wordBreak(String s, Set<String> wordDict) {
+	public static boolean wordBreak(String s, Set<String> wordDict) {
 		if(s==null || s.trim().isEmpty() || wordDict==null || wordDict.isEmpty())
 			return false;
 		
 		s = s.trim();
 		Map<Integer, Integer> map = new HashMap<>();
-		for(int i=0; i<s.length()-1; i++){
-			for(int j=i; j<s.length()-1; j++){
-				if(wordDict.contains(s.substring(i, j))){
+		for(int i=0; i<s.length(); i++){
+			for(int j=i; j<s.length(); j++){
+				if(wordDict.contains(s.substring(i, j+1))){
 					if(map.containsKey(j)){
 						if(i>0 && map.containsKey(i-1))
 							map.put(j, i);
@@ -23,21 +25,25 @@ public class WordBreak {
 				}
 			}
 		}
+		System.out.println(map);
 		
 		if(map.isEmpty())	return false;
 		
 		int end = s.length()-1;
-		while(true){
+		while(end >= 0){
 			if(!map.containsKey(end))	return false;
-			end = map.get(end) - 1;
-			if(end==0)	break;
+			end = map.get(end);		
+			end = end -1;
 		}
 		return true;
     }
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String s = "leetcode";
+		String[] d = {"leet", "code"};
+		Set<String> dict = new HashSet<>(Arrays.asList(d));
+		System.out.println(wordBreak(s, dict));
 	}
 
 }
