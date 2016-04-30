067473bffc494bffbc5ca43459f5ec1b19cd39ff
Asraf <asraf.alom02@gmail.com>
2016-04-30 15:18:21 -0500

    initial commit

---
+++ mysolutions/src/wordBreak/WordBreak.java
@@ -0,0 +1,14 @@
+package wordBreak;
+
+import java.util.Set;
+
+public class WordBreak {
+	public boolean wordBreak(String s, Set<String> wordDict) {
+		return false;
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
