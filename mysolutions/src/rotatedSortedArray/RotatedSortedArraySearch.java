58d37f86e8d04d8db1ff831b86196b234e4f0b23
Asraf <asraf.alom02@gmail.com>
2016-04-30 22:01:07 -0500

    update

--- mysolutions/src/rotatedSortedArray/RotatedSortedArraySearch.java
+++ mysolutions/src/rotatedSortedArray/RotatedSortedArraySearch.java
@@ -24,23 +24,32 @@ public class RotatedSortedArraySearch {
 		return -1;
     }
 	
+	/**
+	 * Find the transition point.
+	 * Values will not be in order in that point
+	 * @param a
+	 * @param s
+	 * @param e
+	 * @return
+	 */
 	private static int findStartPosition(int[] a, int s, int e){
 		if(s > e)	return 0;
 		System.out.println("s: " + s + ", e: " + e);
 		int mid = (s + e)/2;
 		
+		// Values will not be in an order in that point
 		if((mid-1)>=s && (a[mid] < a[mid-1]))	return mid;
 		else if((mid+1)<=e && (a[mid] > a[mid+1]))	return mid+1;
 		
-		if(a[s] > a[mid-1])	return findStartPosition(a, s, mid-1);
+		if((mid-1)>=s && (a[s] > a[mid-1]))	return findStartPosition(a, s, mid-1);
 		else if(a[mid] > a[e])	return findStartPosition(a, mid, e);
 		return 0;
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] a = {7, 0, 1, 2, 4, 5, 6};
-		//int[] a = {1, 2, 4};
+		int[] a = {2, 4, 5, 5, 6, 6, 7, 0, 1};
+		//int[] a = {1};
 		System.out.println(search(a, 2));
 	}
 
